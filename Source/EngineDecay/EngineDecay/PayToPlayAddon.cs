27f19ae32edd2d813b003c371346f7259d1078c2
DarthPointer <al.kiriackin@yandex.ru>
2020-05-05 01:07:45 +0300

    WIP recovery callback

---
+++ Source/EngineDecay/EngineDecay/PayToPlayAddon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineDecay
+{
+    //[KSPAddon(KSPAddon.Startup.Instantly, true)]
+    class PayToPlayAddon
+    {
+        public void Start()
+        {
+            GameEvents.onVesselRecovered.Add(ModuleReader.Read);
+        }
+    }
+
+    public static class ModuleReader
+    {
+        public static void Read(ProtoVessel v, bool wtf)
+        {
+            Vessel vessel = v.vesselRef;
+
+            foreach (Part p in vessel.parts)
+            {
+                EngineDecay engineDecay = p.FindModuleImplementing<EngineDecay>();
+                engineDecay.OnRecovered();
+            }
+        }
+    }
+}
